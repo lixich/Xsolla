a4d045e76525ff3e4cdff7b8791238073d8833ed
lixich <lixich@mail.ru>
2017-07-24 03:50:30 +0500

    Update Brackets.php

--- Brackets.php
+++ Brackets.php
@@ -1,12 +1,13 @@
+<?php
 class Brackets
 {
-  public function isBracketSequenceCorrect($str) {
-   do {
-    $prevStrLen = strlen($str);
-    $str = str_replace("[]","", $str);
-    $str = str_replace("()","", $str);
-    $str = str_replace("{}","", $str);
-   } while (strlen($str) < $prevStrLen);
-   return (strlen($str) == 0) ? true : false;
-  }
+    public function isBracketSequenceCorrect($str) {
+        do {
+            $prevStrLen = strlen($str);
+            $str = str_replace("[]","", $str);
+            $str = str_replace("()","", $str);
+            $str = str_replace("{}","", $str);
+        } while (strlen($str) < $prevStrLen);
+        return (strlen($str) == 0) ? true : false;
+    }
 }
